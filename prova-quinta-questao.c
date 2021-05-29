3236716ad3a8b949ec71130c688e57b4cde020d4
Andressa_SabinoIFCE <84898051+Andressasabino@users.noreply.github.com>
2021-05-29 01:08:05 -0300

    Update prova-quinta-questao.c

--- prova-quinta-questao.c
+++ prova-quinta-questao.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-printf("5. QUESTÃO – ESTRUTURA DE DECISÃO – OPERADOR ? (1,0 PONTO):\n");
-printf("O programa abaixo deveria mostrar o valor absoluto ou módulo de um determinado número fornecido pelo usuário. \nExemplo: se x = -3, o programa deve mostrar resultado 3. Corrija o erro deste programa.\n");
- 
+
 int main()
 {
+    printf("5. QUESTÃO – ESTRUTURA DE DECISÃO – OPERADOR ? (1,0 PONTO):\n");
+    printf("O programa abaixo deveria mostrar o valor absoluto ou módulo de um determinado número fornecido pelo usuário. \nExemplo: se x = -3, o programa deve mostrar resultado 3. Corrija o erro deste programa.\n");
+ 
     int x;
     scanf("%d", &x);
     printf("x=%d\n", x < 0 ? (x * -1) : x);
